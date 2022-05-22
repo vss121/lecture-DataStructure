9c9c2a9baa8c55741449a4ea90adc4098c45085b
vss121 <79561890+vss121@users.noreply.github.com>
2022-05-22 11:46:52 +0900

    Update 2.c

--- coding-test/2.c
+++ coding-test/2.c
@@ -70,7 +70,27 @@ void printList(headNode* h)
 		p = p->link;
 	}
 }
+int invertList(headNode* h) {	//리스트의 링크를 역순으로 재 배치
 
+	if(h->first == NULL) {	//list가 비었을 경우
+		printf("nothing to invert...\n");
+		return 0;	//종료
+	}
+	listNode *n = h->first;	//n을 headnode로 초기화
+	listNode *trail = NULL;	//trail을 널로 초기화
+	listNode *middle = NULL;	//middle을 널로 초기화
+
+	while(n != NULL){	//마지막 노드까지
+		trail = middle;	//trail에 middle 대입
+		middle = n;	//middle에는 n 대입
+		n = n->link;	//n 다음 노드로
+		middle->link = trail;	//middle의 link는 trail과 연결
+	}
+
+	h->first = middle;	//헤드노드는 middle과 연결
+
+	return 0;
+}
 int main()
 {
 	headNode* headnode = NULL;
