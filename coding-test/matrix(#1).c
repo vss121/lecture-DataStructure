6dd8fef88b14b3a6b0129798b369a7a82c34073a
vss121 <79561890+vss121@users.noreply.github.com>
2022-05-23 13:33:22 +0900

    Update matrix(#1).c

--- coding-test/matrix(#1).c
+++ coding-test/matrix(#1).c
@@ -53,3 +53,34 @@ int main()
     return 0;
 
 }
+
+/*
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int row,col;
+	scanf("%d %d", &row, &col);
+	
+	int** matrix = (int **)malloc(sizeof(int)*row);
+	for(int r=0; r<row; r++) {
+		matrix[r] = (int *)malloc(sizeof(int)*col);
+	}
+	
+	int idx=1;
+	for(int r=0; r<row; r++) {
+		for(int c=0; c<col; c++) {
+			matrix[r][c] = idx;
+			printf("%d ",matrix[r][c]);
+			idx++;
+		}
+		printf("\n");
+	}
+	
+	for(int r=0; r<row; r++) {
+		free(matrix[r]);
+	}
+	free(matrix);
+}
+*/
