26cff59372d48f2541941e1ec5d00d7aa9ba8aa3
vss121 <79561890+vss121@users.noreply.github.com>
2022-05-24 12:32:46 +0900

    Update BST.c

--- coding-test/BST.c
+++ coding-test/BST.c
@@ -1,91 +1,90 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node {
+typedef struct node{
 	int key;
 	struct node* left;
 	struct node* right;
 } Node;
 
-void initialize(Node** h)
+int initialize(Node** h)
 {
 	(*h) = (Node*)malloc(sizeof(Node));
 	(*h)->key = -9999;
-	(*h)->left = NULL;	//사용
 	(*h)->right = (*h);
+	(*h)->left = NULL;
 }
-void freenode(Node* ptr)
+int preorder(Node* ptr)
+{
+	if(ptr) {
+		printf("%d ", ptr->key);	//
+		preorder(ptr->left);
+		preorder(ptr->right);
+	}
+}
+int inorder(Node* ptr)
+{
+	if(ptr) {
+		
+		inorder(ptr->left);
+		printf("%d ", ptr->key);
+		inorder(ptr->right);
+	}
+}
+int postorder(Node* ptr)
+{
+	if(ptr) {
+		
+		postorder(ptr->left);
+		postorder(ptr->right);
+		printf("%d ", ptr->key);
+	}
+}
+int freenode(Node* ptr)
 {
 	if(ptr) {
 		freenode(ptr->left);
-		freenode(ptr->right);
-		free(ptr);
+		freenode(ptr->right);	//**
+		free(ptr);	//
 	}
 }
-int freeBST(Node* h)
+int freeBST(Node * h)
 {
-	Node* p = h->left;	//root node
-	freenode(p);
+	Node* temp = h->left;
+	freenode(temp);
 	free(h);
 }
-int insert(Node* h, int num)
+int insertnode(Node* h, int num)
 {
-	
 	Node* newnode = (Node*)malloc(sizeof(Node));
 	newnode->key = num;
-	newnode->left = NULL;
 	newnode->right = NULL;
+	newnode->left = NULL;
 	
-	if(h->left == NULL) {
+	if(h->left==NULL) {
 		h->left = newnode;
-		return 1;
+		return 1; 	//**
 	}
 	
-	Node* ptr = h->left;	//root node
-	Node* parent = NULL;
+	Node* ptr = h->left;
+	Node* parent = NULL;	//**
 	while(ptr) {
-		parent = ptr;
-		if(ptr->key < num) ptr = ptr->right;
-		else ptr = ptr->left;
+		parent = ptr;	//**
+		if(ptr->key > num) ptr = ptr->left;	//
+		else if (ptr->key < num) ptr = ptr->right;
 	}
-	if(parent->key > num) parent->left = newnode;
-	else parent->right = newnode;
+	if(parent->key > num) parent->left = newnode;	//
+	else if(parent->key < num) parent->right = newnode;	//
 }
-Node* searchrecursive(Node* ptr, int num)
+Node* searchrecursive(Node* ptr, int num)	//
 {
 	if(ptr->key > num) ptr = searchrecursive(ptr->left, num);
 	else if(ptr->key < num) ptr = searchrecursive(ptr->right, num);
 	return ptr;
 }
-void inorderTraversal(Node* ptr)	//중위 순회
-{
-	if(ptr) {	//ptr이 널이 아니면
-		inorderTraversal(ptr->left);	//왼쪽 서브 트리 이동
-		printf("%d ", ptr->key);	//현재 노드 처리
-		inorderTraversal(ptr->right);	//오른쪽 서브 트리 이동
-	}
-}
-
-void preorderTraversal(Node* ptr)	//전위 순회
-{
-	if(ptr) {	//ptr이 널이 아니면
-		printf("%d ", ptr->key);	//현재 노드 처리
-		preorderTraversal(ptr->left);	//왼쪽 서브 트리 이동
-		preorderTraversal(ptr->right);	//오른쪽 서브 트리 이동
-	}
-}
-
-void postorderTraversal(Node* ptr)	//후위 순회
-{
-	if(ptr) {	//ptr이 널이 아니면
-		postorderTraversal(ptr->left);	//왼쪽 서브 트리 이동
-		postorderTraversal(ptr->right);	//오른쪽 서브 트리 이동
-		printf("%d ", ptr->key);	//현재 노드 처리
-	}
-}
 int main()
 {
-	Node* head = NULL; 
+	Node* head = NULL;
 	initialize(&head);
 	
 	int arr[5];
@@ -93,13 +92,14 @@ int main()
 		scanf("%d", &arr[i]);
 	}
 	for(int i=0; i<5; i++) {
-		insert(head, arr[i]);
+		insertnode(head, arr[i]);
 	}
 	
-	inorderTraversal(head->left);
+	preorder(head->left);
 	printf("\n");
-	preorderTraversal(head->left);
+	inorder(head->left);
 	printf("\n");
-	postorderTraversal(head->left);
+	postorder(head->left);
+	
 	freeBST(head);
 }
