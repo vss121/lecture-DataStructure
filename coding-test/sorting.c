2e61dd2bd173878fe24ebe15c5a4f8be339fb4c5
vss121 <79561890+vss121@users.noreply.github.com>
2022-06-13 08:32:51 +0900

    Update sorting.c

--- coding-test/sorting.c
+++ coding-test/sorting.c
@@ -1,3 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_ARRAY_SIZE 13
+
+int selectionSort(int *arr)
+{
+	int min;
+	int minidx;
+	
+	for(int i=0; i<MAX_ARRAY_SIZE; i++) {
+		minidx = i;
+		min = arr[i];	
+		
+		for(int j=i+1; j<MAX_ARRAY_SIZE; j++) {
+			if(min > arr[j]) {
+				min = arr[j];
+				minidx = j;
+			}
+		}
+		arr[minidx] = arr[i];	//위치
+		arr[i] = min;
+	}
+}
+
+int insertionSort(int *a)	//re
+{
+	int i, j, t;
+	
+	for(i=1; i<MAX_ARRAY_SIZE; i++) {
+		t=a[i];
+		j=i;
+		while(a[j-1]>t && j>0)
+		{
+			a[j]=a[j-1];
+			j--;
+		}
+		a[j] = t;
+	}
+	
+}
+
+int quickSort(int *a, int n) {	//re
 	int v, t;
 	int i, j;
 
@@ -26,3 +70,50 @@
 		quickSort(a, i);	//왼쪽 부분집합을 정렬
 		quickSort(a+i+1, n-i-1);	//오른쪽 부분집합을 정렬
 	}
+}
+
+int bubbleSort(int *a)	//버블 정렬
+{
+	int t;
+	
+	for(int i=0; i< MAX_ARRAY_SIZE; i++) {
+		for(int j=0; j<MAX_ARRAY_SIZE-i-1; j++) {
+			if(a[j]>a[j+1]) {
+				t = a[j];
+				a[j] = a[j+1];
+				a[j+1] = t;
+			} 
+		}
+	}
+}
+
+int main() {
+	
+	srand(time(NULL));
+	int *array = NULL;
+	array = (int*)malloc(sizeof(int)*MAX_ARRAY_SIZE);
+	
+	for(int i=0; i< MAX_ARRAY_SIZE; i++) {	//initialize
+		array[i] = rand() % MAX_ARRAY_SIZE;
+	}
+	
+	for(int i=0; i< MAX_ARRAY_SIZE; i++) {	//print
+		printf("%5d", array[i]);
+	}
+	printf("\n");
+	
+	//selectionSort(array);
+	
+	insertionSort(array);
+	
+	//bubbleSort(array);
+	
+	//quickSort(array, MAX_ARRAY_SIZE);
+	
+	for(int i=0; i< MAX_ARRAY_SIZE; i++) {	//print
+		printf("%5d", array[i]);
+	}
+	
+	free(array);
+		
+}
